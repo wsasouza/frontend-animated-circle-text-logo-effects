b4e38bf26c065e0e4b156635b318b8aeda59dabe
wsasouza <wsasouza@hotmail.com>
2021-10-03 22:54:22 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const text = document.querySelector(".text p");
+
+text.innerHTML = text.innerText
+  .split("")
+  .map(
+    (char, i) =>
+      `<span style="transform:rotate(${i * 7.5}deg)">
+        ${char}
+    </span>`
+  )
+  .join("");
